387b6c118d9dab98501dc371e27015f731a34d2b
hskim881028 <hskim881028@gmail.com>
2025-03-07 00:22:34 +0900

    feat: Add solution

---
+++ src/Domir.Server/Services/MyFirstService.cs
@@ -0,0 +1,21 @@
+﻿using Domir.Shared;
+using MagicOnion;
+using MagicOnion.Server;
+
+namespace Domir.Server.Services;
+
+public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService
+{
+    // `UnaryResult<T>` allows the method to be treated as `async` method.
+    public async UnaryResult<int> SumAsync(int x, int y)
+    {
+        Console.WriteLine($"Received:{x}, {y}");
+        return x + y;
+    }
+
+    public async UnaryResult<int> TastAsync(int x)
+    {
+        Console.WriteLine($"Received:{x}");
+        return x;
+    }
+}
